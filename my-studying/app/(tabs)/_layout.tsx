186a11f6dbd8fcd399283108a2ace1ac115598db
Saeka03 <143541044+Saeka03@users.noreply.github.com>
2026-04-29 12:12:38 +0900

    Create a header (#1)

--- my-studying/app/(tabs)/_layout.tsx
+++ my-studying/app/(tabs)/_layout.tsx
@@ -1,35 +1,48 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import { useRouter } from "expo-router";
+import { Pressable, StyleSheet, Text } from "react-native";
+import React from "react";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { HapticTab } from "@/components/haptic-tab";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  const router = useRouter();
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
+        headerShown: true,
         tabBarButton: HapticTab,
-      }}>
+
+        headerStyle: {
+          backgroundColor: "#C2E1C2",
+        },
+
+        headerTintColor: "#778472",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          title: "",
+
+          headerLeft: () => (
+            <Pressable onPress={() => router.push("/")}>
+              <Text style={styles.title}>MY ANKI</Text>
+            </Pressable>
+          ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  title: {
+    color: "#778472",
+    fontSize: 24,
+    fontWeight: "600",
+    marginLeft: 16,
+    letterSpacing: 0.5,
+  },
+});
